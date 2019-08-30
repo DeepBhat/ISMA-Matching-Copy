a290e7aad7bab3cb31fca7ef3e952063cafef6cd
Deepansh Bhatia <deepbhatiaansh@gmail.com>
2019-08-29 21:25:41 -0500

    Added distance scores

--- main.py
+++ main.py
@@ -7,14 +7,12 @@ Created on Tue Aug 27 16:17:44 2019
 # =============================================================================
 # TO DO:
 # 1. correct the encoding for nearest landmark
-# 2. fix mentor data types
 # 3. drop na
 # =============================================================================
 
 
 
 import pandas as pd
-import numpy as np
 from person import Person
 
 
@@ -31,6 +29,9 @@ if __name__ ==  '__main__':
     mentors = [None for i in range(mentors_df.shape[0])]
     
     # Data Preprocessing
+    
+    # Cleaning nan values
+    
     
     # Encoding categorical values
     codes = {"Do you identify more as an introvert or an extrovert?" : {"Introvert" : -1, 
@@ -49,12 +50,6 @@ if __name__ ==  '__main__':
                                             "Junior" : 0,
                                             "Sophomore" : -1,
                                             "Freshman" : -2},
-             "What's the closest landmark to your home?" : {"Northgate" : 0,
-                                                            "Park West" : 1,
-                                                            "HEB (Texas Avenue, College Station)" : 2,
-                                                            "Walmart (Texas Avenue)" : 3,
-                                                            "Post Oak Mall" : 4,
-                                                            "Downtown Bryan" : 5},
              "How close do you want to be with your mentor?" : {"Talking daily, hanging out multiple times per week" : 2,
                                                                 "Talking often, hanging out roughly once per week" : 1,
                                                                 "Talking sometimes, hanging out every few weeks" : -1,
@@ -82,8 +77,7 @@ if __name__ ==  '__main__':
     mentees_df["Do you like going to sports games?"] -= 3
     
     
-    
-    # Initializing objects
+    # Initialize objects
     for i, row in enumerate(mentees_df.values):
         name, languages, social, gender, gender_preference, time, year,\
         landmark, inside, outside, go_out, travel, sports, close = row
@@ -91,8 +85,7 @@ if __name__ ==  '__main__':
         mentees[i] = Person(name, languages, social, gender, gender_preference, time, year,
                            landmark, inside, outside, go_out, travel, sports, close)
         
-# ===============Fix the data types of mentors first=====================
-#        
+
     for i, row in enumerate(mentors_df.values):
          name, languages, social, gender, gender_preference, time, year,\
          landmark, inside, outside, go_out, travel, sports, close = row
@@ -100,9 +93,25 @@ if __name__ ==  '__main__':
          mentors[i] = Person(name, languages, social, gender, gender_preference, time, year,
                             landmark, inside, outside, go_out, travel, sports, close)
 
-        
-    final_score_df = pd.DataFrame(columns = [mentee.name for mentee in mentees], index = [mentor.name for mentor in mentors])
+    # Initializing the dataframe
+    scores_df = pd.DataFrame(columns = [mentee.name for mentee in mentees], 
+                                  index = [mentor.name for mentor in mentors])
     
+    # Dictionary to hold the distance scores
+    distance_scores = {"Northgate" : {"Northgate" : 10, "Park West" : 7, "HEB (Texas Avenue, College Station)" : 6,
+                                      "Walmart (Texas Avenue)" : 4, "Post Oak Mall" : 4, "Downtown Bryan" : 4},
+                       "Park West" : {"Northgate" : 7, "Park West" : 10, "HEB (Texas Avenue, College Station)" : 7,
+                                      "Walmart (Texas Avenue)" : 5, "Post Oak Mall" : 4, "Downtown Bryan" : 1},
+                       "HEB (Texas Avenue, College Station)" : {"Northgate" : 6, "Park West" : 7, "HEB (Texas Avenue, College Station)" : 10,
+                                      "Walmart (Texas Avenue)" : 8, "Post Oak Mall" : 8, "Downtown Bryan" : 2},
+                       "Walmart (Texas Avenue)" : {"Northgate" : 4, "Park West" : 5, "HEB (Texas Avenue, College Station)" : 8,
+                                      "Walmart (Texas Avenue)" : 10, "Post Oak Mall" : 6, "Downtown Bryan" : 0},
+                       "Post Oak Mall" : {"Northgate" : 4, "Park West" : 4, "HEB (Texas Avenue, College Station)" : 8,
+                                      "Walmart (Texas Avenue)" : 6, "Post Oak Mall" : 10, "Downtown Bryan" : 1},
+                       "Downtown Brayan" : {"Northgate" : 4, "Park West" : 1, "HEB (Texas Avenue, College Station)" : 2,
+                                      "Walmart (Texas Avenue)" : 0, "Post Oak Mall" : 1, "Downtown Bryan" : 10}}
+    
+    # Calculate the scores
     for mentor in mentors:
         for mentee in mentees:
             # Introversion/Extroversion
@@ -129,9 +138,12 @@ if __name__ ==  '__main__':
             # close
             close_score = mentor.close * mentee.close + 4
             
-            final_score_df.loc[mentor.name, mentee.name] = social_score + year_score + inside_score +\
+            # distance
+            distance_score = distance_scores[mentor.landmark][mentee.landmark]
+            
+            scores_df.loc[mentor.name, mentee.name] = social_score + year_score + inside_score +\
                                                        outside_score + go_out_score + travel_score +\
-                                                       sports_score + close_score
+                                                       sports_score + close_score + distance_score
                                                        
 
 
